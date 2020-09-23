4466c9a15ae2673fa822b95ee108a314ebb41789
electrolyte-orb <55880176+electrolyte-orb@users.noreply.github.com>
2020-09-23 20:13:47 +0530

    adding material design ui

---
+++ app.js
@@ -0,0 +1,5 @@
+import { MDCRipple } from "@material/ripple";
+import { MDCTextField } from "@material/textfield";
+
+const textField = new MDCTextField(document.querySelector(".mdc-text-field"));
+const buttonRipple = new MDCRipple(document.querySelector(".mdc-button"));
